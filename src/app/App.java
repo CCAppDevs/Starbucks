52de360133b9302c1e1f79f2e77a06a2a2070ea2
Jesse Harlan <jesse.harlan@centralia.edu>
2025-05-29 12:33:10 -0700

    added panels and panel layouts

--- src/app/App.java
+++ src/app/App.java
@@ -23,7 +23,11 @@ public class App extends JFrame {
 	private JPanel leftPanel;
 	private JPanel rightPanel;
 	private JPanel footerPanel;
-
+	private GridBagConstraints constraints;
+	private GridLayout leftLayout;
+	private BoxLayout rightLayout;
+	private BorderLayout footerLayout;
+	
 	public App() {
 		super("Starbucks App");
 		
@@ -35,6 +39,9 @@ public class App extends JFrame {
 		setMinimumSize(new Dimension(800,600));
 		setSize(new Dimension(1024,768));
 
+		constraints = new GridBagConstraints();
+
+		
 		loadComponents();
 		
 		// make the app visible to users
@@ -43,18 +50,68 @@ public class App extends JFrame {
 	}
 	
 	private void loadComponents() {
+
+		
+		
+		
 		// load panels
 		this.leftPanel = new JPanel();
 		this.leftPanel.setBackground(Color.GREEN);
+		leftLayout = new GridLayout();
+		leftLayout.setRows(3);
+		leftLayout.setColumns(3);
+		this.leftPanel.setLayout(leftLayout);
+		
+		
+		
 		this.rightPanel = new JPanel();
 		this.rightPanel.setBackground(Color.BLUE);
+		rightLayout = new BoxLayout(this.rightPanel, BoxLayout.Y_AXIS);
+		this.rightPanel.setLayout(rightLayout);
+		
+		
 		this.footerPanel = new JPanel();
 		this.footerPanel.setBackground(Color.RED);
+		footerLayout = new BorderLayout();
+		this.footerPanel.setLayout(footerLayout);
 		
-		// add the panels
-		add(this.leftPanel);
-		add(this.rightPanel);
-		add(this.footerPanel);
+		// TODO: This is not laying out in a 3x3 grid
+		this.leftPanel.add(new JButton("1"));
+		this.leftPanel.add(new JButton("2"));
+		this.leftPanel.add(new JButton("3"));
+		this.leftPanel.add(new JButton("4"));
+		this.leftPanel.add(new JButton("5"));
+		this.leftPanel.add(new JButton("6"));
+		this.leftPanel.add(new JButton("7"));
+		this.leftPanel.add(new JButton("8"));
+		this.leftPanel.add(new JButton("9"));
+		
+		this.rightPanel.add(new JLabel("Stuff"));
+		
+		// TODO: fix layout, buttons are overlapping
+		this.footerPanel.add(new JButton("Clear"));
+		this.footerPanel.add(new JLabel("Price: $0.00"));
+		this.footerPanel.add(new JButton("Calculate"));
+		
+		// TODO: all panels should fill the available space
+		
+		// add the panels		
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		add(this.leftPanel, constraints);
+		
+		
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		add(this.rightPanel, constraints);
+		
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.gridwidth = 2;
+		add(this.footerPanel, constraints);
 	}
 
 	public static void main(String[] args) {
